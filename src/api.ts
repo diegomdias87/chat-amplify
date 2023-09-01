ce286dfee9fd77c8042999191b775919d4a900b7
diegomdias87 <74568161+diegomdias87@users.noreply.github.com>
2023-09-01 09:34:40 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,27 @@
 export const getChatResponse = async (prompt: string): Promise<string> => {
-  console.log('Prompt sent:', prompt); // Step 2: Add this line for logging
+  // Step 2: Add this line for logging
+  console.log('Prompt sent:', prompt);
 
+  const response = await fetch("https://926ljsicr2.execute-api.us-east-1.amazonaws.com/dev", {
+    method: 'GET', // Change this to a GET request
+    // Optionally, you can include headers if needed
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   // Remove 'x-api-key' header
+    //   // 'x-api-key': API_KEY,
+    // },
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch response from API.');
+  }
+
+  const data = await response.json();
+  return data.response;
+};
+
+// Add another function for making POST requests if needed
+export const sendPrompt = async (prompt: string): Promise<string> => {
   const response = await fetch("https://926ljsicr2.execute-api.us-east-1.amazonaws.com/dev", {
     method: 'POST',
     headers: {
@@ -15,6 +36,6 @@ export const getChatResponse = async (prompt: string): Promise<string> => {
     throw new Error('Failed to fetch response from API.');
   }
 
-  const data = await response;
+  const data = await response.json();
   return data.response;
 };
